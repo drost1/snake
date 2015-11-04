b7a4c89efa7e7a0b4dec961d1384fc9d099c3712
drost1 <drost@inbox.ru>
2015-11-05 00:21:12 +0300

    Добавил новые классы, урок 9. Надо доделать

---
+++ Snake/Snake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point p, int length, Direction direction)
+        {
+            
+        }
+        public void move(int length, Direction direction)
+        {
+            if(direction == Direction.LEFT)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
